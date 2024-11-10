77a0a8a39aacad2428ac0df06ffc3fd61444f60e
Georgi Hadzhiev <georgi.hadzhiev@abv.bg>
2024-11-10 09:39:48 +0200

    fully implemented the method for getting the directional input from the player

--- SimpleSnake/Core/Engine.cs
+++ SimpleSnake/Core/Engine.cs
@@ -1,12 +1,14 @@
 ﻿using SimpleSnake.Core.Interfaces;
+using SimpleSnake.Enums;
 using SimpleSnake.GameObjects;
+using System;
 
 
 namespace SimpleSnake.Core
 {
     internal class Engine : IEngine
     {
-
+        private Direction direction;
         private Point[] pointsOfDirection;
 
         public Engine()
@@ -16,9 +18,9 @@ namespace SimpleSnake.Core
 
         public void Run()
         {
+            
 
-          
-          
+            
         }
         private void CreateDirection()
         {
@@ -26,6 +28,43 @@ namespace SimpleSnake.Core
             pointsOfDirection[1] = new Point(-1, 0);
             pointsOfDirection[2] = new Point(0, 1);
             pointsOfDirection[3] = new Point(0, -1);
+        }
+        private void GetNextDirection()
+        {
+
+            ConsoleKeyInfo userInput = Console.ReadKey();
+
+            if(userInput.Key == ConsoleKey.LeftArrow)
+            {
+                if(direction != Direction.Right)
+                {
+                    direction = Direction.Left;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.RightArrow)
+            {
+                if (direction != Direction.Left)
+                {
+                    direction = Direction.Right;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.UpArrow)
+            {
+                if (direction != Direction.Down)
+                {
+                    direction = Direction.Up;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.DownArrow)
+            {
+                if (direction != Direction.Up)
+                {
+                    direction = Direction.Down;
+                }
+            }
+
+            Console.CursorVisible = false;
+
         }
     }
 }
